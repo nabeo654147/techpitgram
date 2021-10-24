433416a97eba09163d0eaf8e0d6dba05895b45c9
渡邉智彦 <watanabetomohiko@watanabetomohikonoMacBook-Air.local>
2021-10-24 21:04:55 +0900

    【Ruby on Rails 6】Instagram風簡易SNSアプリを作ってみよう！ 8章_ コメント機能

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,11 @@
+import 'bootstrap';
+import '../stylesheets/application';
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
